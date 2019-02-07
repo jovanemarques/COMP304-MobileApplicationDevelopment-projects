d5e5b9c656dbecb6a59793aeae38d1ec1126d65f
Jovane Marques <jovanemarques@gmail.com>
2019-02-07 01:29:14 -0500

    Assignment 2

--- JovaneMarques_COMP304_003_Assign2/app/src/main/java/com/github/jovanemarques/jovanemarques_comp304_003_assign2/TicketTypeActivity.java
+++ JovaneMarques_COMP304_003_Assign2/app/src/main/java/com/github/jovanemarques/jovanemarques_comp304_003_assign2/TicketTypeActivity.java
@@ -1,7 +1,9 @@
 package com.github.jovanemarques.jovanemarques_comp304_003_assign2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class TicketTypeActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class TicketTypeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_type);
+
+        Spinner spin = findViewById(R.id.spAdults);
+
+        ArrayAdapter<CharSequence> adapt = ArrayAdapter.createFromResource(this,
+                R.array.tickets_quantity, android.R.layout.simple_spinner_item);
+        adapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spin.setAdapter(adapt);
     }
 }
